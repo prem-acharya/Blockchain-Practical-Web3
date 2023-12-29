3550f964c4d4873945a0db4f69ff1a9c860b88ae
Prem Acharya <102874190+prem-acharya@users.noreply.github.com>
2023-12-29 10:49:14 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 
-eth = "https://sepolia.infura.io/v3/cc73bc9930e94fd49e2a0b0eddbf26e2"
+eth = "https://mainnet.infura.io/v3/cdebf3b29acc4544a64b7be348b0b84a"
 
 web3 = Web3(Web3.HTTPProvider(eth))
 
